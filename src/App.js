1e1d90e0f33e8d6cda69d2eaeba71d943533832c
dfirebaugh <dafirebaugh@gmail.com>
2017-11-02 03:16:15 -0400

    added wall collisions

--- src/App.js
+++ src/App.js
@@ -6,29 +6,69 @@ import Map from './Components/Map.js';
 class App extends Component {
   constructor(props){
 		super(props)
-		this.state = {playerDirection:"!", nX:16,nY:0, playerPos:[16,0]}
+		this.state = {playerDirection:"!",collide:false,nX:16,nY:0, playerPos:[16,0]}
+    this.collideHandler = this.collideHandler.bind(this);
+    this.getMap = this.getMap.bind(this);
   }
   componentDidMount(){
     document.body.addEventListener('keydown', (e) => {
-
       if(e.key === "ArrowDown"||e.key === "ArrowUp"||e.key === "ArrowLeft"||e.key === "ArrowRight"){
         this.setState({playerDirection:e.key});
       }
 
       if(e.key === "ArrowDown"){
-        this.setState({nY:this.state.nY+1});
-      }
-      if(e.key === "ArrowUp"){
-        this.setState({nY:this.state.nY-1});
-  		}
-  		if(e.key === "ArrowLeft"){
-        this.setState({nX:this.state.nX-1})
+        if(this.state.map[this.state.nY+1][this.state.nX] !== '▒'){
+          this.setState({nY:this.state.nY+1});
         }
-  		if(e.key === "ArrowRight"){
-        this.setState({nX:this.state.nX+1});
       }
 
+      if(e.key === "ArrowUp"){
+        if(this.state.map[this.state.nY-1][this.state.nX] !== '▒'){
+          this.setState({nY:this.state.nY-1});
+        }
+      }
+      if(e.key === "ArrowLeft"){
+        if(this.state.map[this.state.nY][this.state.nX-1] !== '▒'){
+          this.setState({nX:this.state.nX-1})
+        }
+      }
+      if(e.key === "ArrowRight"){
+        if(this.state.map[this.state.nY][this.state.nX+1] !== '▒'){
+          this.setState({nX:this.state.nX+1});
+        }
+      }
+
+
+
 		})
+  }
+  collideHandler(direction,nX,nY){
+    this.setState({collide:true})
+    var collideY = nY;
+    var collideX = nX;
+
+    if(direction=== "ArrowDown"){
+      collideY -= 1;
+      // this.setState({nY:this.state.nY-1});
+    }
+    if(direction=== "ArrowUp"){
+      collideY +=1;
+      // this.setState({nY:this.state.nY+1});
+    }
+    if(direction=== "ArrowLeft"){
+      collideX +=1;
+      // this.setState({nX:this.state.nX+1})
+    }
+    if(direction=== "ArrowRight"){
+      collideX -= 1;
+      // this.setState({nX:this.state.nX-1});
+    }
+    this.setState({nX:collideX});
+    this.setState({nY:collideY});
+    this.setState({collide:false})
+  }
+  getMap(arr){
+    this.setState({map: arr})
   }
 
   render() {
@@ -40,7 +80,7 @@ class App extends Component {
         <p className="App-intro">
 
         </p>
-        <Map playerDirection={this.state.playerDirection} nX={this.state.nX} nY={this.state.nY} playerPos = {this.state.playerPos}/>
+        <Map playerDirection={this.state.playerDirection} getMap={this.getMap} onCollide={this.collideHandler} nX={this.state.nX} nY={this.state.nY} playerPos = {this.state.playerPos}/>
       </div>
     );
   }
--- src/Components/Map.js
+++ src/Components/Map.js
@@ -3,7 +3,6 @@ import Grid from './Grid.js';
 import generate from './generate.js';
 import Enemies from './Enemies.js';
 import Items from './Items.js';
-import update from 'react-addons-update';
 
 
 const GRID_HEIGHT = 25;
@@ -17,18 +16,47 @@ class Map extends React.Component{
 	constructor(props){
 		super(props)
 		this.state = {curLevel:[],playerDirection:this.props.playerDirection,pX:16,pY:0,pH:100,pAtt:10}
-	}
+		}
 	componentDidMount(){
 		var arr = generate(GRID_HEIGHT,GRID_WIDTH,MAX_ROOMS,ROOM_SIZE_RANGE);
 		Enemies(arr,GRID_WIDTH,GRID_HEIGHT);
 		Items(arr,GRID_WIDTH,GRID_HEIGHT);
 
+		this.props.getMap(arr);
 		this.updateLevel(arr);
 	}
 	componentWillReceiveProps(newProps){
-		this.setState({playerDirection:newProps.playerDirection});
+		// console.log(newProps)
 		this.movePlayer(newProps);
+
+		var b = this.state.pX;
+		var a = this.state.pY;
+		if(a === newProps.nY && b === newProps.nX){
+		}else{
+
+			this.cleanCell(a,b);
 		}
+		// console.log('cur: ' + newProps.nY + ' ' + newProps.nX)
+		// console.log('prev: ' + a + ' ' + b)
+
+
+		// var curPos = this.state.curLevel[newProps.nY][newProps.nX];
+		//
+		// console.log("newPropsX: " + newProps.nX + " Y:" + newProps.nY)
+		// if(curPos === '▒'){
+		// 	console.log('COLLIDE!');
+		// 	this.props.onCollide(newProps.playerDirection, newProps.nX, newProps.nY)
+		// 	}
+		// else{
+		// 	this.movePlayer(newProps);
+		// 	if(curPos === '#'){
+		// 		console.log('ENEMY!')
+		// 	}
+		// 	if(curPos === '^'){
+		// 		console.log('ITEM!');
+		// 	}
+		// }
+	}
 	updateLevel(arr){
 		var level = this.state.curLevel.slice();
 		arr[0][16] = 'P';
@@ -45,20 +73,22 @@ class Map extends React.Component{
 		this.setState({curLevel:arr});
 	}
 	movePlayer(newProps){
-		var b = this.state.pX;
-		var a = this.state.pY;
-		this.setState({pX:newProps.nX});
-		this.setState({pY:newProps.nY});
-
+		// console.log(this.state.curLevel[newProps.nY][newProps.nX])
 		var levelArr = this.state.curLevel.slice();
-		levelArr[newProps.nY][newProps.nX] = 'P';
-		this.setState({curLevel:levelArr});
-		this.cleanCell(a,b)
+		if(levelArr.length > 1){
+			this.setState({pX:newProps.nX});
+			this.setState({pY:newProps.nY});
+			levelArr[newProps.nY][newProps.nX] = 'P';
+			this.setState({curLevel:levelArr});
+		}
+
 	}
 	cleanCell(a,b){
 		var levelArr = this.state.curLevel.slice();
-		levelArr[a][b] = ' ';
-		this.setState({curLevel:levelArr});
+		if(levelArr.length > 1){
+			levelArr[a][b] = ' ';
+			this.setState({curLevel:levelArr});
+	}
 	}
 	render(){
 		return(
